2ffe24bead42572e3c57d52b26e0b879e451cca8
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-08-06 23:36:46 +0530

    it is working for both the cases case1 and case2 but fun fact it only run for 2/126

--- LargestCommonPrefix.c
+++ LargestCommonPrefix.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
-#include <string.h>
+
+// Function prototype
+char* longestCommonPrefix(char** strs, int strsSize);
+
+int main() {
+
+    // Modifiable strings
+    char str1[] = "";
+    char str2[] = "";
+    char str3[] = "";
+
+    char *strs[] = {""
+        
+    };
+
+    int strsSize = 0;
+
+    char *sub = longestCommonPrefix(strs, strsSize);
+
+    printf("Longest Common Prefix: %s\n", sub);
+
+    return 0;
+}
 
 char* longestCommonPrefix(char** strs, int strsSize) {
-    for(int i=0;i<=strsSize;i++)
-    {
-        i
+
+    int let = 0;
+
+    for (int i = 0; strs[0][i] != '\0'; i++) {
+
+        int count = 0;
+
+        for (int n = 0; n < strsSize - 1; n++) {
+
+            if (strs[n][i] == strs[n + 1][i]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        if (count == strsSize - 1)
+            let++;
+        else
+            break;
     }
-    
-    
+
+    strs[0][let] = '\0';
+
+    return strs[0];
 }
