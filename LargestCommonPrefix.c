e3d6a374c062ac3fc07224c5deedee76134473e1
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-08-07 00:50:28 +0530

    now it is working for user input to and it can find largest Common Prefix for every case

--- LargestCommonPrefix.c
+++ LargestCommonPrefix.c
@@ -5,16 +5,29 @@ char* longestCommonPrefix(char** strs, int strsSize);
 
 int main() {
 
+    char input[100][100];
+    char *strs[100];
+    int strsSize;
+    printf("enter the size of the strings:\n");
+    scanf("%d", &strsSize);
+
+    printf("enter strings :\n");
+    for(int a=0;a<strsSize;a++){
+        scanf("%99s",input[a]);
+        strs[a]=input[a];
+    }
     // Modifiable strings
-    char str1[] = "";
-    char str2[] = "";
-    char str3[] = "";
+    /*char str1[] = "flower";
+    char str2[] = "flow";
+    char str3[] = "flight";
 
-    char *strs[] = {""
-        
-    };
+    char *strs[] = {
+        str1,
+        str2,
+        str3
+    };*/
 
-    int strsSize = 0;
+    
 
     char *sub = longestCommonPrefix(strs, strsSize);
 
