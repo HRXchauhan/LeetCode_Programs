3300cc3447e91b92de71790c1e2f8941f9979787
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-08-08 02:33:05 +0530

    know it is working properly and i beat 100% in runtime and 97.99% in space:)

--- ValidParentheses.c
+++ ValidParentheses.c
@@ -1,68 +1,73 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-
-bool isValid(char* s) {
-
-    char op[100];
-
-    int n=0;
-    for(int i=0;s[i]!='\0';i++){
-        if(s[i]=='(','[','{'){
-
-            op[n]=s[i];
-            n++; 
-
-        }
-        else if(s[i]==')'){
-            if(op[n-1]=='('){
-                op[n-1]='\0';
-                n--;
-            }
-            else
-                return false;
-        }
-        else if(s[i]==']'){
-            if(op[n-1]=='['){
-                op[n-1]='\0';
-                n--;
-            }
-            else return false;
-        }
-        else if(s[i]=='}'){
-            if(op[n-1]=='{'){
-                op[n-1]='\0';
-                n--;
-            }
-            else return false;
-        }
-    }
-
-    if(op[n]=='\0'){
-        return true;
-    }
-    else return false;
-    
-}
+bool isValid(char* s);
 
 int main() {
-
+    
     
     char s[100];
-    int strsSize;
-    printf("enter the size of the strings:\n");
-    scanf("%d", &strsSize);
+    //int strsSize;
+   // printf("enter the size of the strings:\n");
+    //scanf("%d", &strsSize);
 
     printf("enter strings :\n");
-    for(int a=0;a<strsSize;a++){
-        scanf("%99s",s[a]);
-        
-    }
+    
+        scanf("%99s",s);
+       
     bool is= isValid(s);
     if(is){
         printf("true");
     }
     else
         printf("false");
+        
     return 0;
+}
+
+bool isValid(char* s) {
+    int len = strlen(s);
+    char op[len+1];
+
+    int n=0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='('||s[i]=='['||s[i]=='{'){
+            n++;
+            op[n]=s[i];
+            
+
+        }
+        else if(s[i]==')'){
+            if(op[n]=='('){
+                op[n]='\0';
+                n--;
+            }
+            else
+                return false;
+        }
+        else if(s[i]==']'){
+            if(op[n]=='['){
+                op[n]='\0';
+                n--;
+            }
+            else return false;
+        }
+        else if(s[i]=='}'){
+            if(op[n]=='{'){
+                op[n]='\0';
+                n--;
+            }
+            else return false;
+        }
+        else{
+            printf("enter valid string:");
+        }
+    }
+
+    if(op[1]=='\0'){
+        return true;
+    }
+    else return false;
+    
 }
