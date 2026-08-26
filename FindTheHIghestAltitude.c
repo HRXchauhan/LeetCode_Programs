b47b05f5499afa57ea98f7896ed56936b112e4fb
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-08-26 23:12:34 +0530

    complete code

--- FindTheHIghestAltitude.c
+++ FindTheHIghestAltitude.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int largestAltitude(int* gain, int gainSize) {
     int high=0, sum[gainSize+1];
     sum[0]=0;
@@ -8,4 +10,18 @@ int largestAltitude(int* gain, int gainSize) {
         }
     }
     return high;
+}
+
+int main(){
+    int gainSize=0;
+    printf("Enter the size of the gain array:\n");
+    scanf("%d",&gainSize);
+    int gain[gainSize];
+    printf("Enter the gain array:\n");
+    for(int i=0;i<gainSize;i++){
+        scanf("%d",&gain[i]);
+    }
+    
+    int high=largestAltitude(gain, gainSize);
+    printf("The highest Altitude is :->%d",high);
 }
