839e0fdc35f176c9f1f9b3d24955f8589abaf505
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-08-31 22:21:05 +0530

    adding main function

--- IntersectionOfTowArray.c
+++ IntersectionOfTowArray.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
@@ -17,4 +18,30 @@ int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* ret
     }
     *returnSize= n;
     return arr;
+}
+
+int main(){
+    int nums1Size, nums2Size, size;
+    printf("Enter the size of nums1 array:\n");
+    scanf("%d",&nums1Size);
+    printf("enter the size of nums2 array:\n");
+    scanf("%d",&nums2Size);
+    int nums1[nums1Size];
+    int nums2[nums2Size];
+    printf("Enter the nums1 array:\n");
+    for(int i=0;i<nums1Size;i++){
+        scanf("%d",&nums1[i]);
+    }
+    for(int j=0;j<nums2Size;j++){
+        scanf("%d",&nums2[j]);
+    }
+    int *returnSize=size;
+    
+    int *arr=intersection( nums1, nums1Size, nums2, nums2Size, returnSize);
+    for(int e=0;e<size;e++)
+    {
+        printf("ans:-> %d",arr[e]);
+    }
+    free(arr);
+    return 0;
 }
