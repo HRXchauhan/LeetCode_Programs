db27a6d676d42a33b84006a392e696645d670bf5
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-09-02 23:41:35 +0530

    first XOR question

---
+++ XORopreationinarray.c
@@ -0,0 +1,12 @@
+int xorOperation(int n, int start) {
+    int arr[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+       arr[i] = start + 2 * i;
+       
+       sum=sum^arr[i];
+        
+    }
+    return sum;
+    
+}
