f46f5056dd2e4598ffe9e4e707cff198ab6b6587
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-09-06 23:28:28 +0530

    main function for previous function

--- ConvertBinaryNumberInALinkedListtoInteger.c
+++ ConvertBinaryNumberInALinkedListtoInteger.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct ListNode {
+       int val;
+       struct ListNode *next;
+    };
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -19,4 +29,41 @@ int getDecimalValue(struct ListNode* head) {
     }
     return sum;
   
+}
+
+int main(){
+    int size,value;
+    printf("Enter the size of binary nums:\n");
+    scanf("%d",&size);
+    
+    struct ListNode *head = NULL;
+    struct ListNode *tem = NULL;
+    struct ListNode *newnode;
+    
+    for(int i=0;i<size;i++){
+        newnode =malloc(sizeof(struct ListNode));
+        scanf("%d",&value);
+        newnode->val=value;
+        newnode->next=NULL;
+
+        if(head==NULL){
+            head=newnode;
+            tem=head;
+
+        }
+        else{
+            tem->next=newnode;
+            tem=tem->next;
+        }
+    }
+    int result=0;
+    result=getDecimalValue(head);
+
+    printf("The result is:->%d",result);
+
+
+    return 0;
+
+
+    
 }
