a7b343ac3c7fe4d3f99e0578a726fc7b45f421e9
harsh kumar <100089177+HRXchauhan@users.noreply.github.com>
2026-09-11 20:13:05 +0530

    gpt

---
+++ sqrtX.c
@@ -0,0 +1,17 @@
+int mySqrt(int x) {
+    int low = 0, high = x;
+    int ans = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if ((long long)mid * mid <= x) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
